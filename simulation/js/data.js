f1766fece876cf18ef624b67183461aa6839a319
vleadadmin <admin@vlabs.ac.in>
2026-03-28 07:25:16 +0000

    https://virtual-labs.github.io/exp-optimization-steepest-descent-iitb click on the link to test your code.

---
+++ simulation/js/data.js
@@ -0,0 +1,12 @@
+let act1_table_data1;
+let a = 0;
+let b = 0;
+let alpha = 0;
+let x = 0;
+let itr1_fdx = 0;
+let itr1_x = 0;
+let itr2_fdx = 0;
+let itr2_x = 0;
+let act1_table_data_graph;
+let minfxValue;
+//# sourceMappingURL=data.js.map
